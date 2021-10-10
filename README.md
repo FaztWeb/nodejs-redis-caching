55d724e716852c9c5fb7a3eef94c39f88c26a7b3
fazt <fazt@faztweb.com>
2021-10-10 18:02:55 -0500

    basic changes

---
+++ README.md
@@ -0,0 +1,13 @@
+### Redis Node Caching
+
+this is a basic example of how to use Redis with nodejs as a caching system to improve response speed to the client.
+
+### Requirements
+
+you need to have redis installed on your system, or other possible ways to use it is trough docker:
+
+to run redis with docker:
+
+```shell
+docker run --name some-redis -p 6379:3379 -d redis
+```
--- src/index.js
+++ src/index.js
@@ -1,58 +1,80 @@
-const express = require('express')
-const axios = require('axios')
-const redis = require('redis')
-const responseTime = require('response-time');
-const { promisify } = require('util');
+const express = require("express");
+const axios = require("axios");
+const redis = require("redis");
+const responseTime = require("response-time");
+const { promisify } = require("util");
 
-const app = express()
+const app = express();
 
+// Connecting to redis
 const client = redis.createClient({
-    host: '127.0.0.1',
-    port: 6379
-})
+  host: "127.0.0.1",
+  port: 6379,
+});
 
+// Promisifying Get and set methods
 const GET_ASYNC = promisify(client.get).bind(client);
 const SET_ASYNC = promisify(client.set).bind(client);
 
-app.use(responseTime())
+app.use(responseTime());
 
-app.get('/character', async(req, res, next) => {
-    try {
-        const reply = await GET_ASYNC('jokes');
-        if (reply) {
-            console.log('using cached data');
-            return res.send(JSON.parse(reply))
-        }
+// Get all characters
+app.get("/character", async (req, res, next) => {
+  try {
+    // Search Data in Redis
+    const reply = await GET_ASYNC("character");
 
-        const response = await axios.get('https://rickandmortyapi.com/api/character')
-        const saveResult = await SET_ASYNC('jokes', JSON.stringify(response.data), 'EX', 10);
+    // if exists returns from redis and finish with response
+    if (reply) return res.send(JSON.parse(reply));
 
-        console.log('saved data:', saveResult);
+    // Fetching Data from Rick and Morty API
+    const response = await axios.get(
+      "https://rickandmortyapi.com/api/character"
+    );
 
-        res.send(response.data)
-    } catch (error) {
-        res.send(error.message)
-    }
+    // Saving the results in Redis. The "EX" and 10, sets an expiration of 10 Seconds
+    const saveResult = await SET_ASYNC(
+      "character",
+      JSON.stringify(response.data),
+      "EX",
+      10
+    );
+
+    // resond to client
+    res.send(response.data);
+  } catch (error) {
+    res.send(error.message);
+  }
 });
 
-app.get('/character/:id', async (req, res, next) => {
-    try {
-        const reply = await GET_ASYNC(req.params.id);
-        if (reply) {
-            console.log('using cached data');
-            return res.send(JSON.parse(reply))
-        }
+// Get a single character
+app.get("/character/:id", async (req, res, next) => {
+  try {
+    const reply = await GET_ASYNC(req.params.id);
 
-        const response = await axios.get('https://rickandmortyapi.com/api/character/' + req.params.id);
-        const saveResult = await SET_ASYNC(req.params.id, JSON.stringify(response.data), 'EX', 15);
-
-        console.log('saved data:', saveResult);
-
-        res.send(response.data)
-    } catch (error) {
-        console.log(error)
-        res.send(error.message)
+    if (reply) {
+      console.log("using cached data");
+      return res.send(JSON.parse(reply));
     }
-})
 
-app.listen(3000, console.log('server listen on port 3000'))
+    const response = await axios.get(
+      "https://rickandmortyapi.com/api/character/" + req.params.id
+    );
+    const saveResult = await SET_ASYNC(
+      req.params.id,
+      JSON.stringify(response.data),
+      "EX",
+      15
+    );
+
+    console.log("saved data:", saveResult);
+
+    res.send(response.data);
+  } catch (error) {
+    console.log(error);
+    res.send(error.message);
+  }
+});
+
+app.listen(3000);
+console.log("server listen on port 3000");
