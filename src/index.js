d669e279ef631603e10692e2a395bfb613161418
Fazt <fazt@faztweb.com>
2025-01-14 17:22:37 -0500

    Updated with ES6

--- src/index.js
+++ src/index.js
@@ -1,83 +1,80 @@
-const express = require("express");
-const axios = require("axios");
-const { createClient } = require("redis");
-const responseTime = require("response-time");
+import express from 'express'
+import axios from 'axios'
+import { createClient } from 'redis'
+import responseTime from 'response-time'
 
-const app = express();
+const app = express()
 
 // Connecting to redis
 const client = createClient({
   host: "127.0.0.1",
   port: 6379,
-});
+})
 
-app.use(responseTime());
+app.use(responseTime())
 
 // Get all characters
 app.get("/character", async (req, res, next) => {
   try {
-    // Search Data in Redis
-    const reply = await client.get("character");
+    const reply = await client.get("character")
 
-    // if exists returns from redis and finish with response
-    if (reply) return res.send(JSON.parse(reply));
+    if (reply) return res.send(JSON.parse(reply))
 
-    // Fetching Data from Rick and Morty API
-    const response = await axios.get(
+    const { data } = await axios.get(
       "https://rickandmortyapi.com/api/character"
-    );
+    )
 
-    // Saving the results in Redis. The "EX" and 10, sets an expiration of 10 Seconds
     const saveResult = await client.set(
       "character",
-      JSON.stringify(response.data),
+      JSON.stringify(data),
       {
         EX: 10,
       }
-    );
+    )
     console.log(saveResult)
 
-    // resond to client
-    res.send(response.data);
+    res.send(data)
   } catch (error) {
-    res.send(error.message);
+    res.send(error.message)
   }
-});
+})
 
 // Get a single character
 app.get("/character/:id", async (req, res, next) => {
   try {
-    const reply = await client.get(req.params.id);
+    const { id } = req.params
+    const reply = await client.get(id)
 
     if (reply) {
-      console.log("using cached data");
-      return res.send(JSON.parse(reply));
+      console.log("using cached data")
+      return res.send(JSON.parse(reply))
     }
 
-    const response = await axios.get(
-      "https://rickandmortyapi.com/api/character/" + req.params.id
-    );
+    const { data } = await axios.get(
+      `https://rickandmortyapi.com/api/character/${id}`
+    )
+    
     const saveResult = await client.set(
-      req.params.id,
-      JSON.stringify(response.data),
+      id,
+      JSON.stringify(data),
       {
         EX: 15,
       }
-    );
+    )
 
-    console.log("saved data:", saveResult);
+    console.log("saved data:", saveResult)
 
-    res.send(response.data);
+    res.send(data)
   } catch (error) {
-    console.log(error);
-    res.send(error.message);
+    console.log(error)
+    res.send(error.message)
   }
-});
+})
 
-async function main() {
-  await client.connect();
-  app.listen(3000);
-  console.log("server listen on port 3000");
+const main = async () => {
+  await client.connect()
+  app.listen(3000)
+  console.log("server listen on port 3000")
 }
 
-main();
+main()
